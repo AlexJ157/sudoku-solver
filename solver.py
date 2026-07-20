2c2ff02ce46a8b1a22f51404dca8b0875480e8fb
Alex Jose <alex.lf.jose18@gmail.com>
2026-07-20 19:03:28 +0100

    Implement recursive backtracking solver and fix move validation

--- solver.py
+++ solver.py
@@ -50,14 +50,36 @@ def is_valid(board, number, position):
         if value == number:
             return False
         
-    start_row = math.floor(((row + 1) / 3)) * 3
-    start_column = math.floor(((column + 1) / 3)) * 3
+    start_row = math.floor((row / 3)) * 3
+    start_column = math.floor((column / 3)) * 3
 
-    for i in range(start_row, start_row + 4):
-        for j in range(start_column, start_column + 4):
+    for i in range(start_row, start_row + 3):
+        for j in range(start_column, start_column + 3):
             if board[i][j] == number:
                 return False
     return True
 
+def solve(board):
+    empty_position = find_empty(board)
+    if empty_position is None:
+        print("Sudoku Solved")
+        return True
+    
+    row, column = empty_position
+
+    for number in range(1, 10):
+        if is_valid(board, number, empty_position):
+            board[row][column] = number
+            
+            if solve(board):
+                return True
+            board[row][column] = 0
+
+    return False
+
 print_board(board)
+solve(board)
+print_board(board)
+print(solve(board))
+
 
